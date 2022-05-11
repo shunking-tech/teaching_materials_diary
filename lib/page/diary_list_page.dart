80c9e73d5e9359e1f2f6acdf43612e76277a19c5
中村駿輝 <shnktech@gmail.com>
2022-05-11 21:06:49 +0900

    微修正

--- lib/page/diary_list_page.dart
+++ lib/page/diary_list_page.dart
@@ -7,7 +7,6 @@ class DiaryListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
