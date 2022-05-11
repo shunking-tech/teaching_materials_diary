3648b69d1e3c55cc1d00b0a398780c9e63aa4355
中村駿輝 <shnktech@gmail.com>
2022-05-11 21:14:16 +0900

    編集画面をStatefulWidgetに変更

--- lib/page/edit_diary_page.dart
+++ lib/page/edit_diary_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class EditDiaryPage extends StatelessWidget {
+class EditDiaryPage extends StatefulWidget {
   const EditDiaryPage({Key? key}) : super(key: key);
 
+  @override
+  State<EditDiaryPage> createState() => _EditDiaryPageState();
+}
+
+class _EditDiaryPageState extends State<EditDiaryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
