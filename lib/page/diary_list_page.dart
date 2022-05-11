eb8e37948926140146e6565ba29f2c356fae55c7
中村駿輝 <shnktech@gmail.com>
2022-05-11 21:04:57 +0900

    編集画面作成して画面遷移

--- lib/page/diary_list_page.dart
+++ lib/page/diary_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:teaching_materials_diary/page/edit_diary_page.dart';
 
 class DiaryListPage extends StatelessWidget {
   const DiaryListPage({Key? key}) : super(key: key);
@@ -7,6 +8,14 @@ class DiaryListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(
+              builder: (context) => const EditDiaryPage()
+          ));
+        },
+      ),
     );
   }
 }
---
+++ lib/page/edit_diary_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EditDiaryPage extends StatelessWidget {
+  const EditDiaryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("編集"),
+      ),
+    );
+  }
+}
